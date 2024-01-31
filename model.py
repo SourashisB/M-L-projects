7da1820ea2f51c56941accba0f2666ce7de12434
Sourashis.Bhowmik <sourashis.bhowmik@fdmgroup.com>
2024-01-31 10:39:40 +0800

    first commit

---
+++ model.py
@@ -0,0 +1,20 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA, TruncatedSVD
+import matplotlib.patches as mpatches
+import time
+
+# Classifier Libraries
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import collections
+
+
+data  = pd.read_csv('./creditcard.csv')
+print(data.head())
