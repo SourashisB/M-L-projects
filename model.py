dbbd58eb1af5e8a2f2576fe48d6ad6b98a63bb22
Sourashis.Bhowmik <sourashis.bhowmik@fdmgroup.com>
2024-01-31 13:42:27 +0800

    Data processing

--- model.py
+++ model.py
@@ -15,16 +15,61 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 import collections
 
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 
-data  = pd.read_csv('../data/creditcard.csv')
+
+data1  = pd.read_csv('../data/creditcard.csv')
 
 #clear null
-data.isnull().sum().max()
+data1.isnull().sum().max()
 
 #check columns
-print(data.columns)
+
 
 #fine detail
-print('No Frauds', round(data['Class'].value_counts()[0]/len(data) * 100,2), '% of the dataset')
-print('Frauds', round(data['Class'].value_counts()[1]/len(data) * 100,2), '% of the dataset')
+print('No Frauds', round(data1['Class'].value_counts()[0]/len(data1) * 100,2), '% of the dataset')
+print('Frauds', round(data1['Class'].value_counts()[1]/len(data1) * 100,2), '% of the dataset')
+
+std_scaler = StandardScaler()
+rob_scaler = RobustScaler()
+
+data1['scaled_amount'] = rob_scaler.fit_transform(data1['Amount'].values.reshape(-1,1))
+data1['scaled_time'] = rob_scaler.fit_transform(data1['Time'].values.reshape(-1,1))
+data1.drop(['Time','Amount'], axis=1, inplace=True)
+
+scaled_amount = data1['scaled_amount']
+scaled_time = data1['scaled_time']
+
+data1.drop(['scaled_amount', 'scaled_time'], axis=1, inplace=True)
+data1.insert(0, 'scaled_amount', scaled_amount)
+data1.insert(1, 'scaled_time', scaled_time)
+
+
+print(data1.head)
+
+x = data1.drop('Class',axis=1)
+y = data1['Class']
+
+stratkFold = StratifiedKFold(n_splits=5, random_state=None, shuffle=False)
+
+for train_index, test_index in stratkFold.split(x, y):
+    print("Train:", train_index, "Test:", test_index)
+    original_Xtrain, original_Xtest = x.iloc[train_index], x.iloc[test_index]
+    original_ytrain, original_ytest = y.iloc[train_index], y.iloc[test_index]
+    
+original_Xtrain = original_Xtrain.values
+original_Xtest = original_Xtest.values
+original_ytrain = original_ytrain.values
+original_ytest = original_ytest.values
+
+train_unique_label, train_counts_label = np.unique(original_ytrain, return_counts=True)
+test_unique_label, test_counts_label = np.unique(original_ytest, return_counts=True)
+print('-' * 100)
+
+#check if test and train data have similar spread
+print('Label Distributions: \n')
+print(train_counts_label/ len(original_ytrain))
+print(test_counts_label/ len(original_ytest))
 
